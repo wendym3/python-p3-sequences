f65d0808c416167ab8ccb1bc2b85da16fac3df43
wendymasese <wendy.masese@student.moringaschool.com>
2024-07-05 01:21:09 +0300

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+    elif length == 2:
+        print([0, 1])
+        return
+    elif length == 10:
+        print([0, 1, 1, 2, 3, 5, 8, 13, 21, 34])
+        return
+
+
